4840f7e7b3f62ab5d34acfa050971e0fc84dce70
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-10-08 17:07:03 +0200

    create header with  title

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+
+  return (
+    <>
+      <h1 className='title'>Meme Generator</h1>
+    </>
+  )
+}
+
+export default Header

5eaadaf6be2ddf5e38f34e12fe30e2bacf99bfd6
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-10-08 18:06:30 +0200

    add style + call to component memeGenerator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import './App.css'
+import MemeGenerator from './components/MemeGenerator'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MemeGenerator />
     </div>
   )
 }
